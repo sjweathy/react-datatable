7bdedac51d4997f15f7d2f14bbb2e6c51afa5185
Seth Weatherly <SWeatherly@bhmanagement.com>
2023-02-19 17:43:51 -0600

    Need some more work understanding how fetch works. I need to return data to a variable that gets sent to view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,34 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-let users: any;
+
 
 import people from "@/data/people.json";
 import DataTable from "@/ui/datatable";
 
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((data) => {
-        users = data;
-    })
-    .then((users) => console.log(users));
+async function getApiData(){
+    let users;
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((data) => {
+            users = data;
+        })
+        .then((users) => console.log(users));
+    // users = res.json;
+    
+    // console.log(users);
+
+    return users;
+
+
+}
+
+let apiData = getApiData();
+
+// const [users, setUsers] = useState(() => getApiData());
 
 export default function Home() {
     return (
